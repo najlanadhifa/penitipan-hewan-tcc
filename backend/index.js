c33f7bf3fc5c42f3cb451cef57d6ad8d19a9f604
Difa <najlanadhifa1103@gmail.com>
2025-05-24 10:30:17 +0700

    update

--- backend/index.js
+++ backend/index.js
@@ -1,10 +1,17 @@
 import express from "express";
 import cors from "cors";
 import route from "./routes/route.js";
+import "./models/Index.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
+const port = process.env.PORT || 5000;
+
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(route);
 
-app.listen(5000, ()=> console.log('SERVER RUNNING . . .'));
+app.listen(port, () => {
+  console.log(`SERVER JALAN DI PORT ${port}`);
+});
---
+++ backend/models/Index.js
@@ -0,0 +1,16 @@
+import db from "../config/Database.js";
+import DaftarHewan from "./DaftarHewanModel.js";
+
+(async () => {
+  try {
+    await db.authenticate();
+    console.log("Koneksi database berhasil!");
+
+    await db.sync({ alter: true });
+    console.log("Semua tabel berhasil disinkronisasi.");
+  } catch (err) {
+    console.error("Gagal konek DB:", err);
+  }
+})();
+
+export { DaftarHewan };
