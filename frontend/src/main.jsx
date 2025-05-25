525a313970d9cdf17d691c8c9a22220ac1d3c45a
Difa <najlanadhifa1103@gmail.com>
2025-05-25 16:53:46 +0700

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './css/index.css'
-import NotesApp from './pages/Notes.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
