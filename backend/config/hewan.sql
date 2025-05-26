829bdd5ca775756829571992482a072a05f5cce9
Difa <najlanadhifa1103@gmail.com>
2025-05-26 22:51:13 +0700

    update

--- backend/config/hewan.sql
+++ backend/config/hewan.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 25, 2025 at 05:55 AM
+-- Generation Time: May 26, 2025 at 05:47 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.11
 
@@ -31,20 +31,22 @@ CREATE TABLE `daftar_hewan` (
   `id` int(11) NOT NULL,
   `nama_hewan` varchar(255) NOT NULL,
   `bobot_hewan` int(11) NOT NULL,
-  `keterangan_khusus` text DEFAULT NULL,
-  `ras` varchar(255) DEFAULT NULL,
-  `gambar` varchar(255) DEFAULT NULL,
+  `keterangan_khusus` text NOT NULL,
+  `ras` varchar(255) NOT NULL,
+  `gambar` varchar(255) NOT NULL,
   `Tanggal_dibuat` datetime NOT NULL,
-  `Tanggal_diperbarui` datetime NOT NULL
+  `Tanggal_diperbarui` datetime NOT NULL,
+  `userId` int(11) NOT NULL,
+  `pemilikId` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `daftar_hewan`
 --
 
-INSERT INTO `daftar_hewan` (`id`, `nama_hewan`, `bobot_hewan`, `keterangan_khusus`, `ras`, `gambar`, `Tanggal_dibuat`, `Tanggal_diperbarui`) VALUES
-(1, 'ayam', 15, 'makanan harus fresh', 'ayam petelur', 'https://www.putraperkasa.co.id/wp-content/uploads/2023/11/Ayam-New-Hampshire-001.webp', '2025-05-24 03:41:47', '2025-05-24 03:41:47'),
-(2, 'si imhut', 15, 'suka nonton upin ipin', 'ayam malaysia', 'https://www.putraperkasa.co.id/wp-content/uploads/2023/11/Ayam-New-Hampshire-001.webp', '2025-05-24 04:04:27', '2025-05-24 05:05:21');
+INSERT INTO `daftar_hewan` (`id`, `nama_hewan`, `bobot_hewan`, `keterangan_khusus`, `ras`, `gambar`, `Tanggal_dibuat`, `Tanggal_diperbarui`, `userId`, `pemilikId`) VALUES
+(5, 'brownie', 11, 'suka jail', 'chihuahua', 'https://www.purina.co.id/sites/default/files/2021-02/BREED%20Hero_0034_chihuahua_smooth.jpg', '2025-05-26 15:22:31', '2025-05-26 15:45:44', 4, 4),
+(6, 'jerry', 100, 'pemakan segala', 'kampung', 'https://upload.wikimedia.org/wikipedia/commons/b/bb/Gorille_des_plaines_de_l%27ouest_%C3%A0_l%27Espace_Zoologique.jpg', '2025-05-26 15:41:47', '2025-05-26 15:41:47', 4, 3);
 
 -- --------------------------------------------------------
 
@@ -59,23 +61,25 @@ CREATE TABLE `daftar_pemilik` (
   `alamat` text DEFAULT NULL,
   `email` varchar(255) DEFAULT NULL,
   `Tanggal_dibuat` datetime NOT NULL,
-  `Tanggal_diperbarui` datetime NOT NULL
+  `Tanggal_diperbarui` datetime NOT NULL,
+  `userId` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `daftar_pemilik`
 --
 
-INSERT INTO `daftar_pemilik` (`id`, `nama_pemilik`, `no_hp`, `alamat`, `email`, `Tanggal_dibuat`, `Tanggal_diperbarui`) VALUES
-(1, 'dapid', '08123456789', 'sewon', 'dapid@gmail.com', '2025-05-24 05:55:47', '2025-05-24 05:55:47');
+INSERT INTO `daftar_pemilik` (`id`, `nama_pemilik`, `no_hp`, `alamat`, `email`, `Tanggal_dibuat`, `Tanggal_diperbarui`, `userId`) VALUES
+(3, 'kartika', '088888888888', 'jogja', 'kartika143@gmail.com', '2025-05-26 10:06:23', '2025-05-26 11:11:17', 3),
+(4, 'nadhifa', '0812345789', 'sleman', 'nadhifa@gmail.com', '2025-05-26 14:28:38', '2025-05-26 14:28:38', 4);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `user`
 --
 
-CREATE TABLE `users` (
+CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
@@ -85,6 +89,16 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`id`, `username`, `password`, `refresh_token`, `createdAt`, `updatedAt`) VALUES
+(1, 'adminkyut123', '$2b$10$.izJwGMRN5OcML88dr6lHew5OO4d1F4QYNsKG7heGqN.ZuU2hNb5m', NULL, '2025-05-25 11:54:44', '2025-05-26 09:09:27'),
+(2, 'kartika', '$2b$10$OiW5Nw.qmc4MgMasnEzsHOBcDy4MCmhBxL98Q1Dcl4rkHD4fuMKqK', NULL, '2025-05-26 09:17:20', '2025-05-26 14:13:30'),
+(3, '123220205', '$2b$10$n7MqqyN3a7lQkTNfGY2ST.qTN6iV9Db3.V4D0oEGu5MvJDz3xI02W', NULL, '2025-05-26 09:56:01', '2025-05-26 14:27:26'),
+(4, 'najla', '$2b$10$YSfp5KvNVWbuFf1U/h218e6Lll10eEakb5LNcoenVHNBkBHbNJsKS', NULL, '2025-05-26 14:27:54', '2025-05-26 15:46:24');
+
+--
 -- Indexes for dumped tables
 --
 
@@ -92,18 +106,21 @@ CREATE TABLE `users` (
 -- Indexes for table `daftar_hewan`
 --
 ALTER TABLE `daftar_hewan`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userId` (`userId`),
+  ADD KEY `pemilikId` (`pemilikId`);
 
 --
 -- Indexes for table `daftar_pemilik`
 --
 ALTER TABLE `daftar_pemilik`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userId` (`userId`);
 
 --
--- Indexes for table `users`
+-- Indexes for table `user`
 --
-ALTER TABLE `users`
+ALTER TABLE `user`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -114,19 +131,92 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `daftar_hewan`
 --
 ALTER TABLE `daftar_hewan`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `daftar_pemilik`
 --
 ALTER TABLE `daftar_pemilik`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
--- AUTO_INCREMENT for table `users`
+-- AUTO_INCREMENT for table `user`
 --
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `daftar_hewan`
+--
+ALTER TABLE `daftar_hewan`
+  ADD CONSTRAINT `daftar_hewan_ibfk_10` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_hewan_ibfk_12` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_hewan_ibfk_14` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_hewan_ibfk_16` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_hewan_ibfk_18` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_hewan_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_hewan_ibfk_20` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_hewan_ibfk_22` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_hewan_ibfk_24` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_hewan_ibfk_25` FOREIGN KEY (`pemilikId`) REFERENCES `daftar_pemilik` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_hewan_ibfk_4` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_hewan_ibfk_6` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_hewan_ibfk_8` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `daftar_pemilik`
+--
+ALTER TABLE `daftar_pemilik`
+  ADD CONSTRAINT `daftar_pemilik_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_10` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_11` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_12` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_13` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_14` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_15` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_16` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_17` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_18` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_19` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_2` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_20` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_21` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_22` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_23` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_24` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_25` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_26` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_27` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_28` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_29` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_3` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_30` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_31` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_32` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_33` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_34` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_35` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_36` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_37` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_38` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_39` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_4` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_40` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_41` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_42` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_43` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_44` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_45` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_5` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_6` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_7` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_8` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_ibfk_9` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `daftar_pemilik_userId_foreign_idx` FOREIGN KEY (`userId`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
