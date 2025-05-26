f35b0c200983b5df2ce394bc8992c54a3d4a2fc8
Difa <najlanadhifa1103@gmail.com>
2025-05-26 22:45:53 +0700

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import Register from './components/Register';
 import DataHewan from './components/DataHewan';
 import DataPemilik from './components/DataPemilik';
 import EditPemilik from './components/EditPemilik'; 
+import EditHewan from './components/EditHewan'; 
 import Home from './components/Home';
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
       <Route path="/datahewan" element={<DataHewan />} />
       <Route path="/datapemilik" element={<DataPemilik />} />
       <Route path="/datapemilik/:id" element={<EditPemilik />} />
+      <Route path="/datahewan/:id" element={<EditHewan />} />
       <Route path="*" element={<h2 style={{textAlign: "center", marginTop: 50}}>404 - Halaman tidak ditemukan</h2>} />
     </Routes>
     </>
